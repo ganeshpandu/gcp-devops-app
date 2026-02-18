7602762ceb7e1245d45de1aa44e73dbf63df25d6
ganeshpandu <163498070+ganeshpandu@users.noreply.github.com>
2026-02-18 18:08:54 +0530

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const http = require('http');
-const port = process.env.PORT || 8080;
-
-http.createServer((req, res) => {
-  res.end("Cloud Run Production....................................... in us-central1 🚀");
-}).listen(port);
