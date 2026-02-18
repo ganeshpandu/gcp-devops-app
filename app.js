1802433f19b01df16b05e1836399b4ae47de836b
ganeshpandu <163498070+ganeshpandu@users.noreply.github.com>
2026-02-18 15:37:45 +0530

    Update response message in app.js

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const http = require('http');
 const port = process.env.PORT || 8080;
 
 http.createServer((req, res) => {
-  res.end("Cloud Run Production in us-central1 🚀");
+  res.end("Cloud Run Production....... in us-central1 🚀");
 }).listen(port);
